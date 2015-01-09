35012eb90a0f3a76e691db0f19b453bfeda92153
Benjamin Heng <benjheng@gmail.com>
2015-01-10 01:32:52 +0800

    Utilize multiple inheritance to import api functions Pyivle class

--- pyivle/__init__.py
+++ pyivle/__init__.py
@@ -1,72 +1,17 @@
 import api
-import login
-import module
-import consultation
-import rosters_and_groups as roster
-import announcement
-import webcast_lectures 
+from login import Login
+from module import Module
+from consultation import Consultation
+from announcement import Announcement
+from webcast_lectures import WebcastLectures
+from rosters_and_groups import RostersAndGroups
 
-class Pyivle(object):
+class Pyivle(Login, Module, Consultation, RostersAndGroups, Announcement, WebcastLectures):
     def __init__(self, apiKey, authToken=None, **kwargs):
         api.apiKey = apiKey
         if authToken: api.authToken = authToken
         #self.authToken = self._login(userid, password)
 
-        # Login
-        self.validate = login.validate
-        self.username_get = login.username_get
-        self.userid_get = login.userid_get
-        self.useremail_get = login.useremail_get
-
-        # Module
-        self.modules = module.modules
-        self.modules_staff = module.modules_staff
-        self.modules_student= module.modules_student
-        self.module = module.module
-        self.modules_search = module.modules_search
-        self.module_lecturer = module.module_lecturers
-        self.module_information = module.module_information
-        self.module_weblinks = module.module_weblinks
-        self.module_reading_formatted = module.module_reading_formatted
-        self.module_reading_unformatted = module.module_reading_unformatted
-        self.module_readings_formatted_coop = module.module_readings_formatted_coop
-        self.module_reading = module.module_reading
-        self.modules_taken = module.modules_taken
-
-        # Consultation
-        self.consultation_module_facilitators_with_slots = consultation.consultation_module_facilitators_with_slots
-        self.consultation_slots = consultation.consultation_slots
-        self.consultation_signed_up_slots = consultation.consultation_signed_up_slots
-        self.consultation_sign_up = consultation.consultation_sign_up
-        self.consultation_cancel_slot = consultation.consultation_cancel_slot
-        
-        # Rosters & Groups
-        self.class_roster = roster.class_roster
-        self.guest_roster = roster.guest_roster
-        self.groups_by_user_and_module = roster.groups_by_user_and_module
-        self.groups_by_user = roster.groups_by_user
-        self.module_class_groups = roster.module_class_groups
-        self.module_class_group_users = roster.module_class_group_users
-        self.module_official_group_users = roster.module_official_group_users
-        self.class_group_sign_up = roster.class_group_sign_up
-        self.class_group_sign_up_remove = roster.class_group_sign_up_remove
-        self.group_projects_by_user = roster.group_projects_by_user
-        self.project_self_enrol_groups = roster.project_self_enrol_groups
-        self.project_group_users = roster.project_group_users
-        self.project_enrolled_groups = roster.project_enrolled_groups
-        self.project_group_sign_up = roster.project_group_sign_up
-        self.project_group_remove_sign_up = roster.project_group_remove_sign_up
-
-        # Announcement
-        self.announcements = announcement.announcements
-        self.announcements_unread = announcement.announcements_unread
-        self.announcements_add_log = announcement.announcements_add_log
-        self.announcements_add = announcement.announcements_add
-
-        # Webcast Lectures
-        self.webcasts = webcast_lectures.webcasts
-        self.webcasts_add_log = webcast_lectures.webcasts
-        
     def login(self, userid, password):
         api.authToken = api.get_auth_token(api.apiKey, userid, password)
     
